54881c83fe08de07aab9f02c9e6c38d38a3f4641
ststeiger <quandary82@hailmail.net>
2018-02-09 15:44:17 +0100

    Fix EventHandling

--- CoreCMS/TypeScript/EventHandling.ts
+++ CoreCMS/TypeScript/EventHandling.ts
@@ -1,53 +1,203 @@
 
 namespace  JQuery.Replacement 
 {
-    
-    
-    /*
-    $('.feed').on('click', '.feed-item', function (event)
+
+
+    export function Test()
     {
-        // Do something
-    })
-    */
+        let main = document.getElementsByTagName("main")[0];
+
+        //JQuery.Replacement.on(main, "click", "div", function (e)
+        //{
+        //    console.log("on target", e.target);
+        //});
+
+        JQuery.Replacement.subscribeEvent(main, "click", "li", function (e)
+        {
+            console.log("on target", e.target);
+        });
+
+        // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Error
+        // https://developer.mozilla.org/bm/docs/Web/JavaScript/Reference/Errors
+        
+        // https://docs.microsoft.com/en-us/vsts/git/tutorial/history?tabs=visual-studio
+        let ele = document.querySelector("#main > div.checklist > ul > li:nth-child(1)");
+        ele.insertAdjacentHTML("afterend", "<li>Hello</li>");
+
+        // JQuery.Replacement.Test();
+    }
+
+
+    // for all existing or newly added children matching selector in static parent, subscribe event
+    export function subscribeEvent(parentSelector: string | Element
+        , eventName: string
+        , childSelector: string
+        , eventCallback)
+    {
+        if (parentSelector == null)
+            throw new ReferenceError("Parameter parentSelector is NULL");
+
+        if (childSelector == null)
+            throw new ReferenceError("Parameter childSelector is NULL");
+
+        // nodeToObserve: the node that will be observed for mutations
+        let nodeToObserve: Element = <Element>parentSelector;
+        if (typeof (parentSelector) === 'string')
+            nodeToObserve = document.querySelector(<string>parentSelector);
+
+
+        let eligibleChildren: NodeListOf<Element> = nodeToObserve.querySelectorAll(childSelector);
+
+        for (let i = 0; i < eligibleChildren.length; ++i)
+        {
+            eligibleChildren[i].addEventListener(eventName, eventCallback, false);
+        } // Next i 
+
+        // https://stackoverflow.com/questions/2712136/how-do-i-make-this-loop-all-children-recursively
+        function allDescendants(node: Node)
+        {
+            if (node == null)
+                return;
+
+            for (let i = 0; i < node.childNodes.length; i++)
+            {
+                let child = node.childNodes[i];
+                allDescendants(child);
+            } // Next i 
+
+            // IE 11 Polyfill 
+            if (!Element.prototype.matches) Element.prototype.matches = Element.prototype.msMatchesSelector;
+
+            if ((<Element>node).matches)
+            {
+                if ((<Element>node).matches(childSelector))
+                {
+                    // console.log("match");
+                    node.addEventListener(eventName, eventCallback, false);
+                } // End if ((<Element>node).matches(childSelector))
+                // else console.log("no match");
+
+            } // End if ((<Element>node).matches) 
+            // else console.log("no matchfunction");
+
+        } // End Function allDescendants 
+
+
+        // Callback function to execute when mutations are observed
+        let callback:MutationCallback = function (mutationsList: MutationRecord[], observer: MutationObserver)
+        {
+            for (let mutation of mutationsList)
+            {
+                // console.log("mutation.type", mutation.type);
+                // console.log("mutation", mutation);
+
+                if (mutation.type == 'childList')
+                {
+                    for (let i = 0; i < mutation.addedNodes.length; ++i)
+                    {
+                        let thisNode: Node = mutation.addedNodes[i];
+                        allDescendants(thisNode);
+                    } // Next i 
+
+                } // End if (mutation.type == 'childList') 
+                // else if (mutation.type == 'attributes') { console.log('The ' + mutation.attributeName + ' attribute was modified.');
+
+            } // Next mutation 
+
+        }; // End Function callback 
+
+        // Options for the observer (which mutations to observe)
+        let config = { attributes: false, childList: true, subtree: true };
+
+        // Create an observer instance linked to the callback function
+        let observer = new MutationObserver(callback);
+
+        // Start observing the target node for configured mutations
+        observer.observe(nodeToObserve, config);
+    } // End Function subscribeEvent 
+
+
+    // $('.feed').on('click', '.feed-item', function (event)
+    // {
+    //     // Do something
+    // })
+
     
+    // Should be redone with MutationObserver
+    // https://developer.mozilla.org/en-US/docs/Web/API/MutationObserver
+    // https://developer.mozilla.org/en-US/docs/Web/Guide/Events/Mutation_events
+    // https://developer.mozilla.org/en-US/docs/Web/Events/DOMSubtreeModified
     
     // https://stackoverflow.com/questions/15112067/how-does-jquery-on-work
     // https://elliotekj.com/2016/11/05/jquery-to-pure-js-event-listeners-on-dynamically-created-elements/
-    function on(  staticParent: string | HTMLElement
-                , eventName:string
-                , dynamicChildSelector:string
-                , fn)
+    export function on(
+          parentSelector: string | Element
+        , eventName:string
+        , childSelector:string
+        , eventCallback)
     {
-        let parent:HTMLElement =<HTMLElement> staticParent;
-        if (typeof(staticParent) === 'string')
-            parent = document.querySelector(<string>staticParent);
-        
-        let eligibleChildren = parent.querySelectorAll("dynamicChildSelector");
-        
+        if (parentSelector == null)
+            throw new ReferenceError("Parameter parentSelector is NULL.");
+
+        if (childSelector == null)
+            throw new ReferenceError("Parameter childSelector is NULL.");
+
+        // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Error
+        // https://developer.mozilla.org/bm/docs/Web/JavaScript/Reference/Errors
+
+        let parent: Element = <Element>parentSelector;
+        if (typeof (parentSelector) === 'string')
+            parent = document.querySelector(<string>parentSelector);
+
+
+        function isDescendant(parent, child)
+        {
+            let node = child.parentNode;
+            while (node != null)
+            {
+                if (node == parent)
+                {
+                    return true;
+                }
+                node = node.parentNode;
+            } // Whend 
+
+            return false;
+        } // End Function isDescendant 
+
+
         // jQuery().off( eventName );
         parent.addEventListener(eventName,
             function (event)
             {
-                // if (e.target.tagName.toLowerCase() === 'span')
-                // if (event.target.classList.contains(dynamicChildSelector))
-                // fn(event);
-                // https://stackoverflow.com/questions/4706236/how-to-pass-all-arguments-as-collection-to-another-function-and-not-as-single-ar
-                // fn.apply(this, arguments);
+                // if (event.target.tagName.toLowerCase() === 'span')
+                // if (event.target.classList.contains(childSelector))
+                // eventCallback(event);
+                
+                let eligibleChildren: NodeListOf<Element> = parent.querySelectorAll(childSelector);
                 
                 for (let i = 0; i < eligibleChildren.length; ++i)
                 {
-                    if (event.target === eligibleChildren[i])
-                    {
-                        return fn.apply(this, arguments);
-                    }
 
-                }
+                    // if (event.target === eligibleChildren[i])
+                    // {
+                    //     return eventCallback.apply(this, arguments);
+                    // }
+
+                    if (isDescendant(eligibleChildren[i], event.target))
+                    {
+                        // https://stackoverflow.com/questions/4706236/how-to-pass-all-arguments-as-collection-to-another-function-and-not-as-single-ar
+                        return eventCallback.apply(this, arguments);
+                    } // End if (isDescendant(eligibleChildren[i], event.target)) 
+                    
+                } // Next i 
 
                 return null;
-            }
-        );
+            } // End EventHandler 
 
-    }
-    
-    
-}
+        ); // End AddEventListener 
+
+    } // End Function on
+
+
+} // End Namespace 
