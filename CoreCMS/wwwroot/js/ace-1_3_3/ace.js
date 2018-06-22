68ccd2e8e7254ddeca0678f152efb7d9154ba9f5
Me <me@no.spam>
2018-06-22 09:32:03 +0200

    Moving files to correct places

---
+++ CoreCMS/wwwroot/js/ace-1_3_3/ace.js
@@ -0,0 +1,21 @@
+
+// https://www.jvandemo.com/a-10-minute-primer-to-javascript-modules-module-formats-module-loaders-and-module-bundlers/
+// https://spring.io/understanding/javascript-modules
+// http://requirejs.org/docs/api.html
+// https://addyosmani.com/writing-modular-js/
+
+
+// https://github.com/requirejs/r.js/issues/417
+// https://github.com/google/systemjs/blob/master/dist/system.min.js
+
+
+define(function (require, exports, module)
+{
+    "use strict";
+    // console.log(require, exports, module);
+
+    // Hello, Ace editor removed to save space. 
+    // This is a dummy module. 
+    // Source-code editing will be disabled. 
+    return {};
+});
